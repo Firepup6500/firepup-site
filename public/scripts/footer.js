028c128003fb14725c532010bb4845db7d9cd59d
Firepup Sixfifty <firepyp650@gmail.com>
2025-03-27 17:31:48 -0500

    Remove bookie's credit from the footer, that's on the /usage page anyways

--- public/scripts/footer.js
+++ public/scripts/footer.js
@@ -1,7 +1,5 @@
 var foot = document.createElement("footer");
-foot.innerHTML = `<span class="rainbow">This website uses </span><a href="https://bookie0.repl.co" class=cyan>Bookie0's</a> <a href="https://bouncecss.bookie0.repl.co/" class=cyan>BounceCSS</a><span class="rainbow"> for styling.</span>
-${location.pathname !== "/"? '<br/><a href="/" class="cyan">Return to index</a>': ''}
-<br/>
+foot.innerHTML = `${location.pathname !== "/"? '<a href="/" class="cyan">Return to index</a><br/>': ''}
 <span class="rainbow">Website © 2025 Firepup650</span>
 `;
 document.querySelector("body").append(foot);
